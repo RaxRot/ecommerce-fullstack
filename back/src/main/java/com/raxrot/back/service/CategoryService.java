05881b1f0ab2db30e927ae4ea8826062250bc31e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-16 19:33:41 +0100

    add CategoryService

---
+++ back/src/main/java/com/raxrot/back/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.raxrot.back.service;
+
+import com.raxrot.back.dto.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+    List<CategoryDTO> getAllCategories();
+    CategoryDTO getCategoryById(Long id);
+    CategoryDTO updateCategory(Long id, CategoryDTO categoryDTO);
+    void deleteCategory(Long id);
+}
