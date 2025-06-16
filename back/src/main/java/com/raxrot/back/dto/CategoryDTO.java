8240cc39762a95ebaa5904f4cf87e363c5aa478d
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-16 19:22:10 +0100

    add CategoryDTO

---
+++ back/src/main/java/com/raxrot/back/dto/CategoryDTO.java
@@ -0,0 +1,17 @@
+package com.raxrot.back.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Category name must not be blank")
+    @Size(min = 2, max = 50, message = "Category name must be between 2 and 50 characters")
+    private String name;
+}
