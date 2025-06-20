37036c5747d52cc53dad8b654c3c66379cfebf27
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-20 20:19:21 +0100

    add constants

---
+++ back/src/main/java/com/raxrot/back/configuration/AppConfig.java
@@ -0,0 +1,8 @@
+package com.raxrot.back.configuration;
+
+public class AppConfig {
+    public static final String PAGE_NUMBER="0";
+    public static final String PAGE_SIZE="10";
+    public static final String SORT_BY_ID="id";
+    public static final String SORT_ORDER_ASC="asc";
+}
