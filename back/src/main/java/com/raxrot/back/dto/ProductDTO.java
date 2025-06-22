2924d36d237adb313a8ac883187ac66e5087cd29
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-22 21:39:56 +0100

    make Validation

--- back/src/main/java/com/raxrot/back/dto/ProductDTO.java
+++ back/src/main/java/com/raxrot/back/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.raxrot.back.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,21 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class ProductDTO {
     private Long id;
+
+    @NotBlank(message = "Product name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
+
+    @Size(max = 500, message = "Description can't be longer than 500 characters")
+    private String description;
+
     private String image;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity must be zero or positive")
     private Integer quantity;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
-    private BigDecimal specialPrice;
-    private BigDecimal discount;
 }
